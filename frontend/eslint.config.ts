e9696ceb3c62f96a17d52736463bd57f5fc01625
yesi07111 <yeseniavr07111@gmail.com>
2024-11-17 21:56:23 -0500

    infra: re-estructuring frontend with vite-react template

---
+++ frontend/eslint.config.ts
@@ -0,0 +1,58 @@
+import { Linter } from 'eslint'
+import js from '@eslint/js'
+import globals from 'globals'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import typescript from '@typescript-eslint/eslint-plugin'
+import typescriptParser from '@typescript-eslint/parser'
+
+const config: Linter.Config[] = [
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        ...globals.browser
+      },
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+        project: './tsconfig.json'
+      }
+    },
+    settings: {
+      react: {
+        version: '18.3',
+        typescript: true
+      }
+    },
+    plugins: {
+      '@typescript-eslint': typescript as any,
+      react: react as any,
+      'react-hooks': reactHooks as any,
+      'react-refresh': reactRefresh as any
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      ...typescript.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true }
+      ],
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn'
+    }
+  }
+]
+
+export default config
