35569fb180cc646a0c2d3dc28b04512932340ff8
yesi07111 <yeseniavr07111@gmail.com>
2024-11-17 22:17:49 -0500

    feat(FE): auth prototype

---
+++ frontend/src/components/auth/ProtectedRoute.tsx
@@ -0,0 +1,18 @@
+import { Navigate, useLocation } from 'react-router-dom';
+
+interface ProtectedRouteProps {
+    children: React.ReactNode;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+    const isAuthenticated = localStorage.getItem('token');
+    const location = useLocation();
+
+    if (!isAuthenticated) {
+        return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+
+    return <>{children}</>;
+};
+
+export default ProtectedRoute;
