797d3179fd959e462331646a017932833ec64f37
yesi07111 <yeseniavr07111@gmail.com>
2024-11-21 15:59:03 -0500

    fix(BE): Entity User

--- backend/src/Domain/Entities/Auth/User.cs
+++ backend/src/Domain/Entities/Auth/User.cs
@@ -4,5 +4,10 @@ namespace Playground.Domain.Entities.Auth;
 
 public class User : IdentityUser
 {
-
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string FullCode { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdateAt { get; set; }
+    public DateTime DeleteAt { get; set; }
 }
