d425c37516884c0b9ece131c5eae659b56494354
yesi07111 <yeseniavr07111@gmail.com>
2024-11-25 10:36:27 -0500

    feat(BE): add CleanUpUnverifiedUsersEndpoint

---
+++ backend/src/WebApi/Endpoints/Auth/CleanUpUnverifiedUsersEndpoint.cs
@@ -0,0 +1,19 @@
+using FastEndpoints;
+using Playground.Application.Commands.CleanUp;
+using Playground.Application.Commands.Dtos;
+
+namespace Playground.WebApi.Endpoints;
+
+public class CleanUpUnverifiedUsersEndpoint : Endpoint<CleanUpUnverifiedUsersCommand, CleanUpUnverifiedUsersResponse>
+{
+    public override void Configure()
+    {
+        Post("auth/cleanup-unverified-users");
+        AllowAnonymous();
+    }
+
+    public override Task<CleanUpUnverifiedUsersResponse> ExecuteAsync(CleanUpUnverifiedUsersCommand req, CancellationToken ct)
+    {
+        return req.ExecuteAsync(ct);
+    }
+}
