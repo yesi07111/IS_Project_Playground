31327a02fc4ac7a8352295f872ae30640fbb63a5
LauraMarby <lauramartir2@gmail.com>
2024-11-26 22:41:12 -0500

    fix(BE): fix entities id

--- backend/src/Domain/Entities/Activity.cs
+++ backend/src/Domain/Entities/Activity.cs
@@ -6,7 +6,7 @@ namespace Playground.Domain.Entities
 {
     public class Activity
     {
-        public int Id { get; set; } 
+        public Guid Id { get; set; } 
         public string Name { get; set; } 
         public DateTime Date { get; set; } 
         public string Description { get; set; } 
