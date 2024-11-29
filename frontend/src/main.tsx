b414093c10d874aca3ff71390249d0549de738d3
yesi07111 <yeseniavr07111@gmail.com>
2024-11-29 18:21:33 -0500

    feat(FE): update main and index styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,3 +8,16 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 );
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('src/services/cacheService.js')
+      .then((registration) => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch((error) => {
+        console.error('Service Worker registration failed:', error);
+      });
+  });
+}
