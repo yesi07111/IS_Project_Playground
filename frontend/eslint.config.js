6868c2224becb1e0108da1702548ed21b3d9eb36
yesi07111 <yeseniavr07111@gmail.com>
2024-11-29 21:13:34 -0500

    feat(FE): add configs documentation

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,3 +1,23 @@
+/**
+ * Configuración de ESLint para un proyecto TypeScript con React.
+ *
+ * Este archivo configura ESLint, una herramienta de análisis estático de código,
+ * para un proyecto que utiliza TypeScript y React. ESLint ayuda a identificar y
+ * corregir problemas en el código, asegurando buenas prácticas de codificación.
+ * La configuración integra plugins y configuraciones recomendadas, incluyendo
+ * soporte para manejar hooks de React y el refresco de componentes.
+ *
+ * El refresco de componentes es una característica que permite actualizar
+ * componentes de React en tiempo real sin perder su estado, mejorando la
+ * experiencia de desarrollo al proporcionar un feedback inmediato.
+ *
+ * - **ignores**: Excluye la carpeta 'dist' del análisis.
+ * - **extends**: Extiende las configuraciones recomendadas de ESLint y TypeScript.
+ * - **files**: Aplica la configuración a archivos con extensiones .ts y .tsx.
+ * - **languageOptions**: Define la versión de ECMAScript y los globales del navegador.
+ * - **plugins**: Incluye plugins para manejar hooks de React y el refresco de componentes.
+ * - **rules**: Define reglas específicas, como advertencias para exportaciones de componentes.
+ */
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
