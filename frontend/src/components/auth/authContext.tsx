c3ab537bcad7b008374557b0a13b4be8062a2581
yesi07111 <yeseniavr07111@gmail.com>
2024-11-29 21:08:33 -0500

    feat(FE): add auth documentation

--- frontend/src/components/auth/authContext.tsx
+++ frontend/src/components/auth/authContext.tsx
@@ -1,35 +1,57 @@
-import React, { createContext, useContext, useState, ReactNode, useEffect } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 import { authService } from '../../services/authService';
+import { AuthContextType } from '../../interfaces/AuthContextType';
+import { AuthProviderProps } from '../../interfaces/AuthProviderProps';
 
-interface AuthContextType {
-    isAuthenticated: boolean;
-    login: () => void;
-    logout: () => void;
-    canAccessUserType: boolean;
-    setCanAccessUserType: (status: boolean) => void;
-    canAccessVerifyEmail: boolean;
-    setCanAccessVerifyEmail: (status: boolean) => void;
-    isEmailVerified: boolean;
-    checkEmailVerification: () => Promise<void>;
-}
-
-interface AuthProviderProps {
-    children: ReactNode;
-}
-
+/**
+ * Contexto de autenticación que proporciona el estado de autenticación y funciones relacionadas.
+ * 
+ * Este contexto se utiliza para compartir el estado de autenticación y las funciones de inicio
+ * y cierre de sesión en toda la aplicación. Se inicializa como indefinido y se debe usar dentro
+ * de un `AuthProvider`.
+ */
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
+/**
+ * Proveedor de contexto de autenticación.
+ * 
+ * Este componente envuelve a los componentes hijos y proporciona el contexto de autenticación,
+ * que incluye el estado de autenticación, funciones para iniciar y cerrar sesión, y verificación
+ * de correo electrónico.
+ * 
+ * @param {AuthProviderProps} props - Propiedades del proveedor, incluyendo los componentes hijos.
+ * @returns {JSX.Element} El proveedor de contexto de autenticación.
+ */
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [canAccessUserType, setCanAccessUserType] = useState(false);
     const [canAccessVerifyEmail, setCanAccessVerifyEmail] = useState(false);
     const [isEmailVerified, setIsEmailVerified] = useState(false);
 
+    /**
+     * Función para iniciar sesión.
+     * 
+     * Establece el estado de autenticación como verdadero.
+     */
     const login = () => setIsAuthenticated(true);
+
+    /**
+     * Función para cerrar sesión.
+     * 
+     * Establece el estado de autenticación como falso.
+     */
     const logout = () => {
         setIsAuthenticated(false);
     };
 
+    /**
+     * Verifica el estado de verificación del correo electrónico del usuario.
+     * 
+     * Esta función obtiene el token de autenticación, el ID y el nombre de usuario
+     * del almacenamiento local y utiliza el servicio de autenticación para verificar
+     * si el correo electrónico está verificado. Actualiza el estado de verificación
+     * del correo electrónico en consecuencia.
+     */
     const checkEmailVerification = async () => {
         try {
             const token = localStorage.getItem('authToken');
@@ -46,6 +68,12 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
         }
     };
 
+    /**
+     * Efecto que verifica el estado de verificación del correo electrónico cuando el usuario está autenticado.
+     * 
+     * Este efecto se ejecuta cada vez que cambia el estado de autenticación. Si el usuario está autenticado,
+     * llama a la función `checkEmailVerification`.
+     */
     useEffect(() => {
         if (isAuthenticated) {
             checkEmailVerification();
@@ -69,6 +97,15 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
     );
 };
 
+/**
+ * Hook personalizado para acceder al contexto de autenticación.
+ * 
+ * Este hook proporciona acceso al contexto de autenticación y debe ser utilizado
+ * dentro de un `AuthProvider`. Lanza un error si se usa fuera de un `AuthProvider`.
+ * 
+ * @returns {AuthContextType} El contexto de autenticación.
+ * @throws {Error} Si se usa fuera de un `AuthProvider`.
+ */
 // eslint-disable-next-line react-refresh/only-export-components
 export const useAuth = (): AuthContextType => {
     const context = useContext(AuthContext);
