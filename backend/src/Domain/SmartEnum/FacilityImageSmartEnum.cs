0f7601dbd7317b6b24e01984e2a2cffe41704961
yesi07111 <yeseniavr07111@gmail.com>
2025-01-01 20:23:09 -0500

    feat(BE): update FacilityImage SmartEnum

---
+++ backend/src/Domain/SmartEnum/FacilityImageSmartEnum.cs
@@ -0,0 +1,149 @@
+using Ardalis.SmartEnum;
+using System;
+using System.Collections.Generic;
+
+namespace Playground.Domain.SmartEnum
+{
+    public class FacilityImageSmartEnum : SmartEnum<FacilityImageSmartEnum>
+    {
+        public static readonly FacilityImageSmartEnum Taller = new("Taller", 1,
+        new List<string>
+        {
+            "/images/activities/art-workshop-1.jpg",
+            "/images/activities/art-workshop-2.jpg",
+            "/images/activities/art-workshop-3.jpg"
+        });
+
+        public static readonly FacilityImageSmartEnum Sala = new("Sala", 2,
+        new List<string>
+        {
+            "/images/activities/kids-guitar.jpg",
+            "/images/activities/kids-music.jpg"
+        });
+
+        public static readonly FacilityImageSmartEnum Laboratorio = new("Laboratorio", 3,
+        new List<string>
+        {
+            "/images/activities/kid-science-1.jpg",
+            "/images/activities/kid-science-2.jpg",
+            "/images/activities/kid-science-3.jpg"
+        });
+
+        public static readonly FacilityImageSmartEnum Piscina = new("Piscina", 4,
+        new List<string>
+        {
+            "/images/activities/pool-1.jpg",
+            "/images/activities/pool-2.jpg",
+            "/images/activities/pool-3.jpg"
+
+        });
+
+        public static readonly FacilityImageSmartEnum Estudio = new("Estudio", 5,
+        new List<string>
+        {
+            "/images/activities/dance-1.jpg",
+            "/images/activities/dance-2.jpg",
+            "/images/activities/dance-3.jpg"
+        });
+
+        public static readonly FacilityImageSmartEnum Atraccion = new("Atracción", 6,
+        new List<string>
+        {
+            "/images/activities/park-1.jpg",
+            "/images/activities/park-2.jpg",
+            "/images/activities/park-3.jpg"
+        });
+
+        public static readonly FacilityImageSmartEnum Biblioteca = new("Biblioteca", 7,
+        new List<string>
+        {
+            "/images/activities/library-1.jpg",
+            "/images/activities/library-2.jpg",
+            "/images/activities/library-3.jpg"
+        });
+
+        public static readonly FacilityImageSmartEnum Gimnasio = new("Gimnasio", 8,
+        new List<string>
+        {
+            "/images/activities/gym-1.jpg",
+            "/images/activities/gym-2.jpg",
+            "/images/activities/gym-3.jpg"
+        });
+
+        public static readonly FacilityImageSmartEnum Cafeteria = new("Cafetería", 9,
+        new List<string>
+        {
+            "/images/activities/cafe-1.jpg",
+            "/images/activities/cafe-2.jpg",
+            "/images/activities/cafe-3.jpg"
+        });
+
+        public static readonly FacilityImageSmartEnum Zoologico = new("Zoológico", 10,
+        new List<string>
+        {
+            "/images/activities/zoo-1.jpg",
+            "/images/activities/zoo-2.jpg",
+            "/images/activities/zoo-3.jpg"
+        });
+
+        public static readonly FacilityImageSmartEnum JuegosAcuaticos = new("Juegos Acuáticos", 11,
+        new List<string>
+        {
+            "/images/activities/waterplay-1.jpg",
+            "/images/activities/waterplay-2.jpg",
+            "/images/activities/waterplay-3.jpg"
+        });
+
+        public static readonly FacilityImageSmartEnum Parque = new("Parque", 12,
+        new List<string>
+        {
+            "/images/activities/park-1-2.jpg",
+            "/images/activities/park-2-2.jpg",
+            "/images/activities/park-3-2.jpg"
+        });
+
+        // Default image paths
+        private static readonly List<string> DefaultImagePaths = new List<string>
+        {
+            "/images/activities/default-1.jpg",
+            "/images/activities/default-2.jpg",
+            "/images/activities/default-3.jpg",
+            "/images/activities/default-4.jpg",
+            "/images/activities/default-5.jpg",
+            "/images/activities/default-6.jpg",
+            "/images/activities/default-7.jpg",
+            "/images/activities/default-8.jpg",
+            "/images/activities/default-9.jpg",
+            "/images/activities/default-10.jpg",
+            "/images/activities/default-11.jpg",
+        };
+
+        private readonly List<string> _imagePaths;
+
+        private FacilityImageSmartEnum(string name, int value, List<string> imagePaths) : base(name, value)
+        {
+            _imagePaths = imagePaths;
+        }
+
+        public string GetRandomImagePath()
+        {
+            var random = new Random();
+            return _imagePaths[random.Next(_imagePaths.Count)];
+        }
+
+        public static string GetRandomImagePathOrDefault(string facilityType)
+        {
+            try
+            {
+                var facilityImageEnum = FromName(facilityType, ignoreCase: true);
+                return facilityImageEnum.GetRandomImagePath();
+            }
+            catch (ArgumentException)
+            {
+                // Return a random image from the default set if no match is found
+                var random = new Random();
+                return DefaultImagePaths[random.Next(DefaultImagePaths.Count)];
+            }
+        }
+    }
+}
