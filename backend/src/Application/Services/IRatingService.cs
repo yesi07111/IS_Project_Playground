f39720386927178a15d7f041d2ab1b7c55f4046f
yesi07111 <yeseniavr07111@gmail.com>
2025-01-01 20:22:44 -0500

    feat(BE): add RatingService

---
+++ backend/src/Application/Services/IRatingService.cs
@@ -0,0 +1,27 @@
+using Playground.Application.Repositories;
+
+namespace Playground.Application.Services
+{
+    /// <summary>
+    /// Proporciona métodos para calcular calificaciones relacionadas con actividades.
+    /// </summary>
+    public interface IRatingService
+    {
+        /// <summary>
+        /// Calcula la calificación promedio de una actividad específica, redondea a 1 lugar después de la coma por defecto.
+        /// </summary>
+        /// <param name="activity">La actividad para la cual se desea calcular la calificación promedio.</param>
+        /// <param name="reviewRepository">El repositorio de reseñas utilizado para obtener las calificaciones.</param>
+        /// <returns>La calificación promedio de la actividad.</returns>
+        double CalculateAverageRating(Domain.Entities.Activity activity, IRepository<Domain.Entities.Review> reviewRepository);
+
+        /// <summary>
+        /// Calcula la calificación promedio de una actividad específica, con una cantidad de dígitos específicos a redondear.
+        /// </summary>
+        /// <param name="activity">La actividad para la cual se desea calcular la calificación promedio.</param>
+        /// <param name="reviewRepository">El repositorio de reseñas utilizado para obtener las calificaciones.</param>
+        /// <param name="round">El valor utilizado para redondear las calificaciones.</param>
+        /// <returns>La calificación promedio de la actividad.</returns>
+        double CalculateAverageRating(Domain.Entities.Activity activity, IRepository<Domain.Entities.Review> reviewRepository, int round);
+    }
+}
