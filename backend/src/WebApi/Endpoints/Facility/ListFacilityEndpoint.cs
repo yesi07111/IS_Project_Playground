44cbb6b9cbf8e6bba8dac660e951de37cb495b80
yesi07111 <yeseniavr07111@gmail.com>
2025-01-01 20:26:43 -0500

    feat(BE): add Facility and User Endpoints

---
+++ backend/src/WebApi/Endpoints/Facility/ListFacilityEndpoint.cs
@@ -0,0 +1,19 @@
+using FastEndpoints;
+using Playground.Application.Queries.Facility.List;
+using Playground.Application.Queries.Responses;
+
+namespace Playground.WebApi.Endpoints.Facility;
+
+public class ListFacilityEndpoint : Endpoint<ListFacilityQuery, ListFacilityResponse>
+{
+    public override void Configure()
+    {
+        AllowAnonymous();
+        Get("/facility/get-all");
+    }
+
+    public override async Task<ListFacilityResponse> ExecuteAsync(ListFacilityQuery req, CancellationToken ct)
+    {
+        return await req.ExecuteAsync(ct);
+    }
+}
---
+++ backend/src/WebApi/Endpoints/User/ListUserEndpoint.cs
@@ -0,0 +1,19 @@
+using FastEndpoints;
+using Playground.Application.Queries.User.List;
+using Playground.Application.Queries.Responses;
+
+namespace Playground.WebApi.Endpoints.User;
+
+public class ListUserEndpoint : Endpoint<ListUserQuery, ListUserResponse>
+{
+    public override void Configure()
+    {
+        AllowAnonymous();
+        Get("/user/get-all");
+    }
+
+    public override async Task<ListUserResponse> ExecuteAsync(ListUserQuery req, CancellationToken ct)
+    {
+        return await req.ExecuteAsync(ct);
+    }
+}
