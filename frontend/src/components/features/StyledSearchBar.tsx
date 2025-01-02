913975e300b5698cfbaa400fd32d0ed632629726
yesi07111 <yeseniavr07111@gmail.com>
2025-01-01 20:32:28 -0500

    feat(FE): add component StyledSearchBar

---
+++ frontend/src/components/features/StyledSearchBar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { TextField } from '@mui/material';
+import { SearchBarProps } from '../../interfaces/SearchBarProps';
+
+export const SearchBar: React.FC<SearchBarProps> = ({ searchTerm, handleSearchChange, labelText }) => {
+    return (
+        <TextField
+            label={`🔍 Buscar ${labelText}`}
+            variant="outlined"
+            value={searchTerm}
+            onChange={handleSearchChange}
+            sx={{ width: '300px', mr: 2 }}
+        />
+    );
+};
