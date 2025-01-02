e94bfc1408c1795cbba896122123884e2ca9f380
yesi07111 <yeseniavr07111@gmail.com>
2025-01-01 20:33:42 -0500

    feat(FE): add interfaces

---
+++ frontend/src/interfaces/UserFilters.tsx
@@ -0,0 +1,10 @@
+export interface UserFilters {
+    userName?: string;
+    email?: string;
+    emailConfirmed?: string;
+    firstName?: string;
+    lastName?: string;
+    rol?: string;
+    markDeleted?: boolean;
+    useCase: string;
+}
