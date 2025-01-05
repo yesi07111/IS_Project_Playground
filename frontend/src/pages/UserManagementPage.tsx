cababd991433e8433acf7cb1ab64b06479f46b0e
Adrián Hernández <hadrianc02@gmail.com>
2025-01-05 00:53:00 -0500

    feat(FE): Created UserManagementPage

---
+++ frontend/src/pages/UserManagementPage.tsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import { Box, Typography, Button, Dialog, DialogActions, DialogContent, DialogTitle, IconButton } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+
+
+
+export default UserManagementPage;
