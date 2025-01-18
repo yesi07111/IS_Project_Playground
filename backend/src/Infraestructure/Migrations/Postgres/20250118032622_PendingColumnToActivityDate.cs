413fef64b36bbb6d602a6a2870b9307426331a20
LauraMarby <lauramartir2@gmail.com>
2025-01-18 14:29:43 -0500

    feat(BE): BD update

---
+++ backend/src/Infraestructure/Migrations/Postgres/20250118032622_PendingColumnToActivityDate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infraestructure.Migrations.Postgres
+{
+    /// <inheritdoc />
+    public partial class PendingColumnToActivityDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Pending",
+                table: "ActivityDate",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Pending",
+                table: "ActivityDate");
+        }
+    }
+}
