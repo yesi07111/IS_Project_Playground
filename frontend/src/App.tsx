28b2b6c5f7c3315f01eb804249a338b985d76250
yesi07111 <yeseniavr07111@gmail.com>
2025-01-22 01:43:27 -0500

    feat(FE): update configs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
@@ -12,6 +12,14 @@ import ProtectedRoute from './components/auth/ProtectedRoute';
 import ActivitiesPage from './pages/ActivitiesPage';
 import ActivityInfoPage from './pages/ActivityInfoPage';
 import ReviewsPage from './pages/ReviewsPage';
+import ResetPasswordPage from './pages/ResetPasswordPage';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import UserProfilePage from './pages/UserProfilePage';
+import EditUserProfilePage from './pages/EditUserProfilePage';
+import { authService } from './services/authService';
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
+import ReservationsPage from './pages/ReservationPage';
+
 import AdminPage from './pages/AdminPage';
 import UserManagementPage from './pages/UserManagementPage';
 import ResourcesPage from './pages/ResourcesPage';
@@ -69,19 +77,88 @@ const theme = createTheme({
   },
 });
 
-/**
- * Componente principal de la aplicación.
- * 
- * Este componente configura el tema de Material-UI, proporciona el contexto de autenticación
- * y define las rutas de la aplicación utilizando React Router. Incluye la barra de navegación
- * y las páginas principales de la aplicación.
- * 
- * @returns {JSX.Element} El componente principal de la aplicación.
- */
 const App: React.FC = () => {
+  const [clientId, setClientId] = useState<string>('');
+  const [siteKey, setSiteKey] = useState<string>('');
+  const [startOnline, setOnlineState] = useState<boolean>(false);
+  const [useCaptcha, setUseCaptcha] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchClientId = async () => {
+      try {
+        const response = await authService.getGoogleClientId();
+        setClientId(response.clientId);
+      } catch (error) {
+        console.error('Error al obtener el clientId de Google:', error);
+      }
+    };
+
+    const fetchSiteKey = async () => {
+      try {
+        const response = await authService.getReCaptchaSiteKey();
+        setSiteKey(response.siteKey);
+      } catch (error) {
+        console.error('Error al obtener el siteKey de Captcha:', error);
+      }
+    };
+
+    fetchClientId();
+    fetchSiteKey();
+
+    if (clientId.length !== 0) {
+      console.log("Iniciando online...")
+      setOnlineState(true);
+    }
+
+    if (siteKey.length !== 0) {
+      console.log("Usando verificación Captcha.")
+      setUseCaptcha(true);
+    }
+
+    // setOnlineState(false); //Descomentar esta línea fuerza al inicio en modo offline
+    // setUseCaptcha(false); //Esta solo fuerza a que no se use captcha
+
+  }, [clientId, siteKey]);
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <GoogleOAuthProvider clientId={clientId}>
+        <GoogleReCaptchaProvider reCaptchaKey="6LeJmLoqAAAAALMk4lMeGU-9JYWmUuwbDVjt4nsp">
+          <AuthProvider>
+            <Router>
+              <Navbar />
+              <Routes>
+                <Route path="/" element={<HomePage reload={false} />} />
+                <Route path="/register" element={<RegisterPage online={startOnline} useCaptcha={useCaptcha} />} />
+                <Route path="/login" element={<LoginPage online={startOnline} useCaptcha={useCaptcha} />} />
+                <Route path="/activities" element={<ActivitiesPage reload={false} />} />
+                <Route path="/reviews" element={<ReviewsPage reload={false} />} />
+                <Route path="/profile/:id" element={<UserProfilePage />} />
+                <Route path="/edit-profile/:id" element={<EditUserProfilePage />} />
+                <Route path="/activities/:id/:imagePath/:useCase" element={<ActivityInfoPage reload={false} />} />
+                <Route path="/my-reservations/:id" element={<ReservationsPage />}></Route>
+                <Route
+                  path="/verify-email"
+                  element={
+                    <ProtectedRoute redirectTo="/login">
+                      <VerifyEmailPage />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/reset-password"
+                  element={
+                    <ProtectedRoute redirectTo="/login">
+                      <ResetPasswordPage />
+                    </ProtectedRoute>
+                  }
+                />
+              </Routes>
+            </Router>
+          </AuthProvider>
+        </GoogleReCaptchaProvider>
+      </GoogleOAuthProvider>
       <AuthProvider>
         <Router>
           <Navbar />
