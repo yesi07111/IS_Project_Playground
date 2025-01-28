6c587b67a6083f5c5bcb9307413203e5c98f0bdb
yesi07111 <yeseniavr07111@gmail.com>
2025-01-28 02:16:52 -0500

    feat(BE): update services

--- frontend/src/services/activityService.ts
+++ frontend/src/services/activityService.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { ActivitiesFilters } from '../interfaces/Filters';
 import { ActivityData, GetActivityResponse, ListActivityResponse } from '../interfaces/Activity';
-import { ReservationCreationResponse, ReservationFormData } from '../interfaces/Reservation';
 
 const API_URL = 'http://localhost:5117/api';
 
@@ -36,18 +35,6 @@ export const activityService = {
                 throw error.response.data || 'Error al obtener los detalles de la actividad.';
             } else {
                 throw 'Ha ocurrido un error inesperado al obtener los detalles de la actividad.';
-            }
-        }
-    },
-    reserveActivityDate: async (data: ReservationFormData): Promise<ReservationCreationResponse> => {
-        try {
-            const response = await axios.post(`${API_URL}/activity/reserve`, data);
-            return response.data;
-        } catch (error: unknown) {
-            if (axios.isAxiosError(error) && error.response) {
-                throw error.response.data;
-            } else {
-                throw new Error('Ha ocurrido un error inesperado al reservar para esta actividad.');
             }
         }
     },
