5bfe96c7b0504b65815578e7595d432232f50969
Adrián Hernández <hadrianc02@gmail.com>
2025-01-28 21:06:26 -0500

    feat(BE): implemented CreateFacilityCommand.Validator

---
+++ backend/src/Application/Commands/Facility/Create/CreateFacilityCommand.Validator.cs
@@ -0,0 +1,25 @@
+using FastEndpoints;
+using FluentValidation;
+
+namespace Playground.Application.Commands.Facility.Create;
+
+public class CreateFacilityCommandValidator : Validator<CreateFacilityCommand>
+{
+    public CreateFacilityCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("El nombre no puede estar vacío.")
+            .NotNull().WithMessage("El nombre no puede ser nulo.");
+        RuleFor(x => x.Type)
+            .NotEmpty().WithMessage("El tipo de instalación no puede estar vacío.")
+            .NotNull().WithMessage("El tipo de instalación no puede ser nulo.");
+        RuleFor(x => x.UsagePolicy)
+            .NotEmpty().WithMessage("Las políticas de uso no pueden estar vacías.")
+            .NotNull().WithMessage("Las políticas de uso no pueden ser nulas.");
+        RuleFor(x => x.MaximumCapacity)
+            .Must(x => x > 0).WithMessage("La capacidad máxima debe ser mayor que cero");
+        RuleFor(x => x.Location)
+            .NotEmpty().WithMessage("La ubicación no puede estar vacío.")
+            .NotNull().WithMessage("La ubicación no puede ser nulo.");
+    }
+}
