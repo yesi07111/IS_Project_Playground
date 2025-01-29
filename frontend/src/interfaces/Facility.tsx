7dafac6d5532d4d8ecb79793f34f6454b357992b
Adrián Hernández <hadrianc02@gmail.com>
2025-01-28 21:00:34 -0500

    feat(BE): added Facility interfaces

--- frontend/src/interfaces/Facility.tsx
+++ frontend/src/interfaces/Facility.tsx
@@ -10,3 +10,20 @@ export interface Facility {
 export interface ListFacilityResponse {
     result: Facility[] | string[];
 }
+
+export interface FacilityResponse {
+    id: string,
+    name: string,
+    location: string,
+    type: string,
+    usagePolicy: string,
+    maximumCapacity: number,
+}
+
+export interface FacilityFormData {
+    name: string,
+    location: string,
+    type: string,
+    usagePolicy: string,
+    maximumCapacity: number,
+}
