0063d40687aeb437d9d5f87b61af0d9c41e16a2b
Adrián Hernández <hadrianc02@gmail.com>
2025-02-01 15:56:25 -0500

    feat(BE): CreateFacilityCommand.Validator modified

--- backend/src/Application/Commands/Facility/Create/CreateFacilityCommand.Validator.cs
+++ backend/src/Application/Commands/Facility/Create/CreateFacilityCommand.Validator.cs
@@ -9,10 +9,12 @@ public class CreateFacilityCommandValidator : Validator<CreateFacilityCommand>
     {
         RuleFor(x => x.Name)
             .NotEmpty().WithMessage("El nombre no puede estar vacío.")
-            .NotNull().WithMessage("El nombre no puede ser nulo.");
+            .NotNull().WithMessage("El nombre no puede ser nulo.")
+            .MinimumLength(3).WithMessage("El nombre debe tener al menos 3 caracteres.");
         RuleFor(x => x.Type)
             .NotEmpty().WithMessage("El tipo de instalación no puede estar vacío.")
-            .NotNull().WithMessage("El tipo de instalación no puede ser nulo.");
+            .NotNull().WithMessage("El tipo de instalación no puede ser nulo.")
+            .MinimumLength(3).WithMessage("El tipo debe tener al menos 3 caracteres.");
         RuleFor(x => x.UsagePolicy)
             .NotEmpty().WithMessage("Las políticas de uso no pueden estar vacías.")
             .NotNull().WithMessage("Las políticas de uso no pueden ser nulas.");
@@ -20,6 +22,7 @@ public class CreateFacilityCommandValidator : Validator<CreateFacilityCommand>
             .Must(x => x > 0).WithMessage("La capacidad máxima debe ser mayor que cero");
         RuleFor(x => x.Location)
             .NotEmpty().WithMessage("La ubicación no puede estar vacío.")
-            .NotNull().WithMessage("La ubicación no puede ser nulo.");
+            .NotNull().WithMessage("La ubicación no puede ser nulo.")
+            .MinimumLength(3).WithMessage("La ubicación debe tener al menos 3 caracteres.");
     }
 }
