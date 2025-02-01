efcdf94b819065c17f553f58e96a0711ce9f042b
Adrián Hernández <hadrianc02@gmail.com>
2025-02-01 15:56:39 -0500

    feat(BE): UpdateFacilityCommand.Validator modified

--- backend/src/Application/Commands/Facility/Update/UpdateFacilityCommand.Validator.cs
+++ backend/src/Application/Commands/Facility/Update/UpdateFacilityCommand.Validator.cs
@@ -7,7 +7,13 @@ public class UpdateFacilityCommandValidator: Validator<UpdateFacilityCommand>
 {
     public UpdateFacilityCommandValidator()
     {
+        RuleFor(x => x.Name)
+            .MinimumLength(3).WithMessage("El nombre debe tener al menos 3 caracteres.");
+        RuleFor(x => x.Type)
+            .MinimumLength(3).WithMessage("El tipo debe tener al menos 3 caracteres.");
         RuleFor(x => x.MaximumCapacity)
             .Must(x => x > 0).WithMessage("La capacidad máxima debe ser mayor que cero");
+        RuleFor(x => x.Location)
+            .MinimumLength(3).WithMessage("La ubicación debe tener al menos 3 caracteres.");
     }
 }
