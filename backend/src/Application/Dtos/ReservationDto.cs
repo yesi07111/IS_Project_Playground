ed65e4ea027b39f0a8eedb6cdd8646d364b37dd9
Adrián Hernández <hadrianc02@gmail.com>
2025-02-01 22:26:12 -0500

    feat(BE): ReservationDto updated

--- backend/src/Application/Dtos/ReservationDto.cs
+++ backend/src/Application/Dtos/ReservationDto.cs
@@ -1,10 +1,16 @@
+using System.Data.SqlTypes;
+
 namespace Playground.Application.Dtos
 {
     public class ReservationDto
     {
-
+        public string ReservationId { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
         public string ActivityId { get; set; } = string.Empty;
         public string ActivityName { get; set; } = string.Empty;
+        public string ActivityDate { get; set; } = string.Empty;
         public int Amount { get; set; }
         public string Comments { get; set; } = string.Empty;
         public string State { get; set; } = string.Empty;
