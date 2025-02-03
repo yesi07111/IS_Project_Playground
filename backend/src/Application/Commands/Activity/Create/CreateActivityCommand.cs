ec75b33bdcc23a55fe2bdee7f79379fc15b52ff9
LauraMarby <lauramartir2@gmail.com>
2025-02-03 01:52:52 -0500

    feat(BE): CreateActivityCommand changed

--- backend/src/Application/Commands/Activity/Create/CreateActivityCommand.cs
+++ backend/src/Application/Commands/Activity/Create/CreateActivityCommand.cs
@@ -5,13 +5,15 @@ namespace Playground.Application.Commands.Activity.Create;
 
 public record CreateActivityCommand : ICommand<GenericResponse>
 {
+    public string UseCase { get; init; } = string.Empty;
+    public string ActivityId { get; init; } = string.Empty;
     public string Name { get; init; } = string.Empty;
     public string Description { get; init; } = string.Empty;
     public string Educator { get; init; } = string.Empty;
     public string Type { get; init; } = string.Empty;
-    public string? Date { get; init; } 
-    public string? Time { get; init; } 
-    public int RecommendedAge { get; init; } 
+    public string? Date { get; init; }
+    public string? Time { get; init; }
+    public int RecommendedAge { get; init; }
     public string Facility { get; init; } = string.Empty;
     public bool Pending { get; init; } = false;
     public bool Private { get; init; } = false;
